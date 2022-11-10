104422a45a1ad9daf41c3f58ebbe2d3687b1eb6c
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2022-11-10 12:15:11 +0300

    adding and endpoint to view other's user's profile

--- backend/src/routes/users/current-user.ts
+++ backend/src/routes/users/current-user.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import { requireAuth } from "../../middlewares/require-auth";
 import { User } from "../../models/user";
 import { NotAuthorizedError } from "../../errors/not-authorized-error";
+import { BadRequestError } from "../../errors/bad-request-error";
 
 const router = Router();
 
@@ -35,5 +36,32 @@ router.get(
     });
   }
 );
+
+/**
+ * @openapi
+ * /api/v1/users/:id:
+ *   get:
+ *     tags:
+ *        - Auth
+ *     description: Get a user's profile info by id.
+ *     produces:
+ *        - application/json
+ *     consumes:
+ *        - application/json
+ *     responses:
+ *       200:
+ *         description: . Get a user's profile info by id.
+ */
+router.get("/api/v1/users/:id", async (req: Request, res: Response) => {
+  const user = await User.findById(req.params.id).populate("sync_bank");
+
+  if (!user) {
+    throw new BadRequestError("User not found");
+  }
+  res.status(200).json({
+    status: "success",
+    user,
+  });
+});
 
 export { router as CurrentUserRoutes };
