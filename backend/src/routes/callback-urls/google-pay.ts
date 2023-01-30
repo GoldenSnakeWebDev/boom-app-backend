e122ab7a24023667f695414f12b7ccce907b1553
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2023-01-30 14:13:00 +0300

    Add fixed playstore call back

--- backend/src/routes/callback-urls/google-pay.ts
+++ backend/src/routes/callback-urls/google-pay.ts
@@ -1,6 +1,20 @@
 import { Request, Response, Router } from "express";
+import { body } from "express-validator";
+import { updateWalletBalance } from "../../utils/sync-bank";
+import {
+  Transaction,
+  ITransactionType,
+  ITransactionStatus,
+} from "../../models/transaction";
+import { getNextTransaction } from "../../utils/transaction-common";
+import { Notification, NotificationType } from "./../../models/notification";
 
 const router = Router();
+
+export enum ActionType {
+  WITHDRAW = "withdraw",
+  DEPOSIT = "deposit",
+}
 
 /**
  * @openapi
@@ -17,9 +31,61 @@ const router = Router();
  */
 router.post(
   "/api/v1/callback-urls/google-plays-tore",
+  [
+    body("amount")
+      .notEmpty()
+      .withMessage("please provide the amount you are willing to deposit"),
+    body("networkType")
+      .notEmpty()
+      .withMessage(
+        "please provide from which network are you withdrawing from"
+      ),
+    body("actionType")
+      .notEmpty()
+      .withMessage("please provide the action type e.g deposit/withdraw"),
+    body("timestamp").notEmpty().withMessage("please provide timestamp"),
+    body("userId").notEmpty().withMessage("please provide timestamp"),
+  ],
   async (req: Request, res: Response) => {
-    console.log(req.body);
-    res.status(200).json({ success: "success" });
+    let { amount, networkType, timestamp, actionType } = req.body;
+
+    if (timestamp) {
+      timestamp = Date.now();
+    }
+
+    const transactionRef = await getNextTransaction();
+
+    const transaction = Transaction.create({
+      transaction_number: transactionRef,
+      amount,
+      user: req.currentUser?.id!,
+      transaction_type: ITransactionType.DEPOSIT,
+      status: ITransactionStatus.PENDING,
+    });
+
+    // after approve that payments have reached to our bank
+    const syncBankResponse = await updateWalletBalance({
+      userId: req.currentUser?.id!,
+      transaction_type:
+        actionType === ActionType.DEPOSIT
+          ? ITransactionType.DEPOSIT
+          : ITransactionType.WITHDRAW,
+      amount,
+      networkType,
+    });
+
+    await Notification.create({
+      notification_type: NotificationType.BOOM,
+      user: req.currentUser?.id,
+      message: `Successfully ${actionType} ${networkType} ${amount}`,
+      timestamp: new Date(timestamp),
+    });
+
+    res.status(200).json({
+      status: "success",
+      transaction,
+      message: syncBankResponse.message,
+    });
   }
 );
 
